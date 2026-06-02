c23430ee848b44df709fc6e9f378ae96603c2913
Xuanngoc <xuanngoc10a@gmail.com>
2026-06-03 00:05:05 +0700

    update

--- resources/views/admin/layouts/components/footer.blade.php
+++ resources/views/admin/layouts/components/footer.blade.php
@@ -1,34 +1,14 @@
 <footer class="content-footer footer bg-footer-theme">
   <div class="container-xxl">
-                <div
-                  class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
-                  <div class="text-body">
-                    &#169;
-                    <script>
-                      document.write(new Date().getFullYear());
-                    </script>
-                    , made with ❤️ by <a href="https://pixinvent.com" target="_blank" class="footer-link">Pixinvent</a>
-                  </div>
-                  <div class="d-none d-lg-inline-block">
-                    <a href="https://themeforest.net/licenses/standard" class="footer-link me-4" target="_blank"
-                      >License</a
-                    >
-                    <a href="https://themeforest.net/user/pixinvent/portfolio" target="_blank" class="footer-link me-4"
-                      >More Themes</a
-                    >
-
-                    <a
-                      href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation/"
-                      target="_blank"
-                      class="footer-link me-4"
-                      >Documentation</a
-                    >
-
-                    <a href="https://pixinvent.ticksy.com/" target="_blank" class="footer-link d-none d-sm-inline-block"
-                      >Support</a
-                    >
-                  </div>
-                </div>
-              </div>
+    <div class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+      <div class="text-body">
+        &#169; {{ date('Y') }}, phát triển bởi
+        <a href="{{ route('admin.index') }}" class="footer-link fw-medium">Pandio</a>
+      </div>
+      <div class="text-body-secondary d-none d-lg-inline-block">
+        Hệ thống quản lý Wedding Studio
+      </div>
+    </div>
+  </div>
 </footer>
 <!-- / Footer -->
